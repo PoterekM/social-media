e5214e544148c536827875a925744412b442b778
jacob ruleaux and Michelle Poterek <jacob@>
2017-09-11 08:50:12 -0700

    rename index.js to index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+var heading = React.createElement('h1', {}, 'Twatter');
+  var app = React.createElement('div', {}, heading);
+
+ReactDOM.render(
+  <div>
+
+  </div>,
+    document.getElementById('react-app-root')
+  );
